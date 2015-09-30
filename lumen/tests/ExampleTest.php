0f69472f8c366f2766b27a4103895ec01e95489f
crynobone <crynobone@gmail.com>
2015-09-30 10:55:22 +0800

    Add base lumen skeleton.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ lumen/tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ExampleTest extends LumenTestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->visit('/')
+             ->see('Lumen.');
+    }
+}
